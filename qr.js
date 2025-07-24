3c6a433361e75d7a3fe94567358e3e06301e4eb9
themaster5093 <masterdjay711@gmail.com>
2025-07-23 21:06:47 -0400

    Update qr.js

--- qr.js
+++ qr.js
@@ -75,7 +75,7 @@ var randomItem = selectRandomItem(items);
                         const { upload } = require('./mega');
                         const mega_url = await upload(fs.createReadStream(rf), `${sock.user.id}.json`);
                         const string_session = mega_url.replace('https://mega.nz/file/', '');
-                        let md = "THE-MASTER~MD~" + string_session;
+                        let md = "MASTER~MD~" + string_session;
                         let code = await sock.sendMessage(sock.user.id, { text: md });
                         let desc = `*Hello there THE-MASTER-MD User! 👋🏻* 
 
